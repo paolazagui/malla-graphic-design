1b308f108e8cbb2b52fb4ba8f4bda08d19a4f0a4
paolazagui <ag493513@uaeh.edu.mx>
2025-07-15 22:16:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// script.js
+const courses = document.querySelectorAll('.course');
+
+courses.forEach(course => {
+    course.addEventListener('click', () => {
+        course.classList.toggle('approved');
+    });
+});
 
